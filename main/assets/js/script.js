4874b5560f3783e5addc7ef40e022c1d17ceace8
Zubrungus <58639394+Zubrungus@users.noreply.github.com>
2025-01-12 15:49:31 -0600

    Remove remnant code

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -128,16 +128,3 @@ function showColor(receivedColor) {
     RenderColors(receivedColor);
     StoreLastColor(receivedColor);
 }
-
-//Adding open close functionality to save palette button.
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
-
-showButton.addEventListener("click", () => {
-    dialog.showModal();
-});
-
-closeButton.addEventListener("click", () => {
-    dialog.close()
-});
